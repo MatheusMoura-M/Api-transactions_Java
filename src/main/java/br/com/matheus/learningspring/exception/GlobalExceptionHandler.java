20f4f56762141098ce3d0682577390babea4ebf2
Matheus M <matheus150101miranda@gmail.com>
2023-07-20 11:19:34 -0300

    implemented message of errors and exceptions

--- src/main/java/br/com/matheus/learningspring/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/matheus/learningspring/exception/GlobalExceptionHandler.java
@@ -1,17 +1,48 @@
 package br.com.matheus.learningspring.exception;
 
 import java.util.HashMap;
+import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
+            final HttpHeaders headers, final HttpStatusCode status, final WebRequest request) {
+
+        final HashMap<String, HashMap<String, String>> returnObject = new HashMap<>();
+        final HashMap<String, String> allErrorsObject = new HashMap<>();
+
+        final List<ObjectError> allErrorsList = ex.getAllErrors();
+
+        allErrorsList.forEach(error -> {
+            if (error instanceof FieldError) {
+                allErrorsObject.put(((FieldError) error).getField(), error.getDefaultMessage());
+            } else {
+                allErrorsObject.put(error.getObjectName(), error.getDefaultMessage());
+            }
+        });
+
+        returnObject.put("message", allErrorsObject);
+
+        return new ResponseEntity<Object>(returnObject, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler({ AppException.class })
     public ResponseEntity<Object> handleAppException(final AppException ex) {
+
         final HashMap<String, String> returnObject = new HashMap<>();
 
         returnObject.put("message", ex.getMessage());
@@ -21,6 +52,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({ Exception.class })
     public ResponseEntity<Object> handleInternal(final RuntimeException ex) {
+
         final HashMap<String, String> returnObject = new HashMap<>();
 
         returnObject.put("message", "Internal Server Error");
